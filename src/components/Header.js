22ce77ab3660eca747854403f1fefe071f48dc5e
Varun Malaviya <varunmalaviya7@gmail.com>
2023-03-31 20:19:19 -0500

    created Header component that serves as the intro to  the website.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//this component contains the intro to the website
+function Header() {
+    return (
+        <header>
+            <h1>Do You Memba?</h1>
+            <p>Welcome to a game that will test your memory skills! The rules are simple: click on 10 cards without clicking on the same card twice! You will need to remember which cards you have clicked because the cards will reshuffle after every click!</p>
+        </header>
+    )
+}
+
+export default Header
