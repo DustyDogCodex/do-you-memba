15ee51afd6db103fa8cbd9935a8b00a71366e57e
Varun Malaviya <varunmalaviya7@gmail.com>
2023-04-07 20:19:19 -0500

    added images and names as props to the card component. Created a kingsELements array that contains 12 card components with names and images for display.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 //this is the component for the individual cards that will be displayed for the user to click
-function Card(){
+function Card({ name, image }){
     return(
         <div className="card">
-            <img src="#" alt="pokemon" />
-            <span>name?</span>
+            <img src={image} alt="character from king of the hill" />
+            <span>{name}</span>
         </div>
     )
 }
--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -2,21 +2,70 @@
 import Card from "./Card"
 
 function MainDisplay(){
+
+    //object containing character names and images
+    const kingsOfTheHill = [
+        {
+            name: 'Bill Dauterive',
+            image: './images/bill-dauterive.jpeg'
+        },
+        {
+            name: 'Bobby Hill',
+            image: './images/bobby-hill.jpg'
+        },
+        {
+            name: 'Boomhauser',
+            image: './images/boomhauser.jpeg'
+        },
+        {
+            name: 'Cotton Hill',
+            image: './images/cotton-hill.jpg'
+        },
+        {
+            name: 'Dale Gribble',
+            image: './images/dale-gribble.jpg'
+        },
+        {
+            name: 'Hank Hill',
+            image: './images/hank-hill.jpeg'
+        },
+        {
+            name: 'John Redcorn',
+            image: './images/john-redcorn.jpg'
+        },
+        {
+            name: 'Kahn Souphanousinphone',
+            image: './images/kahn-souphanousinphone.jpg'
+        },
+        {
+            name: 'Luanne Platter',
+            image: './images/luanne-platter.jpg'
+        },
+        {
+            name: 'Minh Souphanousinphone',
+            image: './images/Minh_Souphanousinphone.jpg'
+        },
+        {
+            name: 'Nancy Gribble',
+            image: './images/nancy-gribble.jpg'
+        },
+        {
+            name: 'Peggy Hill',
+            image: './images/peggy-hill.jpg'
+        },
+    ]
+
+    //array containging card elements with character images and names
+    const kingsElements = kingsOfTheHill.map(king => {
+        return (
+            <Card name={king.name} image={king.image}/>
+        )
+    })
+
     return(
         <div className="main">
             <div className="card-display">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {kingsElements}
             </div>
             <div className="score-display">
                 <span>Current Score:</span>
